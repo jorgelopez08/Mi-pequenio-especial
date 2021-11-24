81100acae6c799e881f91f05583e95805bce653e
oscar <oscara1706cl@gmail.com>
2021-11-24 16:08:36 -0600

    ready to deploy

--- mi_pequeno/settings.py
+++ mi_pequeno/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = os.getenv("DJANGO_SECRET_KEY_PEQUENO")
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['*']
+ALLOWED_HOSTS = ['127.0.0.1', 'localhost',  'mi-pequeno-especial.herokuapp.com']
 
 
 # Application definition
