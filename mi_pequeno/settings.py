bb0f26686e8b1428e086e98050ebecebf82cba4d
oscar <oscara1706cl@gmail.com>
2021-11-25 16:11:35 -0600

    last change to settings.py

--- mi_pequeno/settings.py
+++ mi_pequeno/settings.py
@@ -141,7 +141,7 @@ USE_TZ = True
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
-
+STATICFILES_STORAGE = "whitenoise.storage.CompressedManifestStaticFilesStorage"
 STATIC_URL = '/static/'
 
 MEDIA_ROOT =  BASE_DIR / 'media'
@@ -149,7 +149,7 @@ STATIC_ROOT =  BASE_DIR / 'staticfiles'
 
 MEDIA_URL = '/media/'
 
-STATICFILES_DIRS =[BASE_DIR / 'static']
+STATICFILES_DIRS =[os.path.join(BASE_DIR, "static")]
 
 STATICFILES_FINDERS = [
     'django.contrib.staticfiles.finders.FileSystemFinder',
