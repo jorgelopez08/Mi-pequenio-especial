b85e8a3b7f07f499c49fac532e361d2890b8f7ee
oscar <oscara1706cl@gmail.com>
2021-11-25 06:58:21 -0600

    probando cosas

--- mi_pequeno/settings.py
+++ mi_pequeno/settings.py
@@ -21,7 +21,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = os.getenv("DJANGO_SECRET_KEY_PEQUENO")
+SECRET_KEY = 'django-insecure-uz(n!02q$7cba%r)v26d&x16l9(4k_&ntrc4)%=eh(eda=frz-'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
